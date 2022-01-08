7cff20b335c8e0a01d3c017649ec133df82235b1
saif_mal1k <saifmalik1200@gmail.com>
2022-01-08 16:51:51 +0530

    project env created , init git

---
+++ converter.py
@@ -0,0 +1,13 @@
+import tkinter
+
+#create a window with a title "docx to md converter"
+window = tkinter.Tk()
+window.title("docx to md converter")
+window.geometry("500x500") #size of window 500x500
+
+
+# open window
+window.mainloop()
+
+
+# add 
